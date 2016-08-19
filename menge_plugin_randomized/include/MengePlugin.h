b55f182a07de2faa8a18fea97bcde1c77baecc83
Michael Huang <bbxxtt2010@gmail.com>
2016-08-19 17:10:37 -0500

    factor out MengePlugin
    Create a reuseable super class responsible for generic
    logic in creating Menge simulation environment.

---
+++ menge_plugin_randomized/include/MengePlugin.h
@@ -0,0 +1,62 @@
+
+/*
+ * MengePlugin.h
+ *
+ *  Created on: Jul 2, 2016
+ *      Author: Michael Huang
+ */
+
+#ifndef MENGEPLUGIN_H_
+#define MENGEPLUGIN_H_
+#include <ignition/math/Pose3.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <gazebo/gazebo.hh>
+#include <gazebo/common/Plugin.hh>
+#include "Agents/BaseAgent.h"
+#include "MyViewer.h"
+#include <SimulatorDB.h>
+#include <SimulatorDBEntry.h>
+#include <SimulatorInterface.h>
+
+namespace gazebo {
+
+class MengePlugin: public WorldPlugin {
+public:
+	void Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf);
+	void OnUpdate(const common::UpdateInfo & _info);
+
+protected:
+	void simLoad();
+	virtual void insertAgentModel(const Menge::Agents::BaseAgent* agt) =0;
+	virtual void insertAgentActor(const Menge::Agents::BaseAgent* agt) =0;
+	void insertAgents();
+	virtual bool updateAgent(const Menge::Agents::BaseAgent* agt);
+	void updateAgents();
+	void updateGoals();
+	void controlActor(physics::ActorPtr _actor);
+	void controlActorAgents();
+	bool modelsInserted();
+	void simStart();
+	void simUpdate(float simTime);
+	int simMain( Menge::SimulatorDBEntry * dbEntry, const std::string & behaveFile,
+			const std::string & sceneFile, const std::string & outFile, const std::string & scbVersion,
+			const std::string & dumpPath ) ;
+
+	Menge::Vis::MyViewer _mengeView;
+	const Menge::Agents::SimulatorInterface * _simulator;
+	void TakeControlOnActor();
+	protected: std::vector<event::ConnectionPtr> connections;
+        transport::NodePtr node;
+	protected: physics::WorldPtr _world;
+	protected: double dt;
+	transport::PublisherPtr factoryPub;
+	
+	protected: ignition::math::Vector3d velocity;
+	protected: double targetWeight = 1.0;
+	protected: common::Time lastUpdate;
+	protected: physics::TrajectoryInfoPtr trajectoryInfo;
+};
+} /* namespace gazebo */
+
+#endif /* MENGEPLUGIN_H_ */
--- menge_plugin_randomized/include/MengeRand.h
+++ menge_plugin_randomized/include/MengeRand.h
@@ -1,5 +1,5 @@
 /*
- * MengePlugin.h
+ * MengeRand.h
  *
  *  Created on: Jul 2, 2016
  *      Author: Michael Huang
@@ -7,54 +7,16 @@
 
 #ifndef MENGERAND_H_
 #define MENGERAND_H_
-#include <ignition/math/Pose3.hh>
-#include <gazebo/physics/physics.hh>
-#include <gazebo/common/common.hh>
-#include <gazebo/gazebo.hh>
-#include <gazebo/common/Plugin.hh>
-#include "Agents/BaseAgent.h"
-#include "MyViewer.h"
-#include <SimulatorDB.h>
-#include <SimulatorDBEntry.h>
-#include <SimulatorInterface.h>
-
+#include "MengePlugin.h"
 namespace gazebo {
 
-class MengeRand: public WorldPlugin {
-public:
-	void Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf);
-	void OnUpdate(const common::UpdateInfo & _info);
+class MengeRand: public MengePlugin {
 
-private:
-	void simLoad();
-	void insertAgentModel(const Menge::Agents::BaseAgent* agt);
-	void insertAgentActor(const Menge::Agents::BaseAgent* agt);
-	void insertAgents();
-	bool updateAgent(const Menge::Agents::BaseAgent* agt);
-	void updateAgents();
-	void updateGoals();
-	void controlActor(physics::ActorPtr _actor);
-	void controlActorAgents();
-	bool modelsInserted();
-	void simStart();
-	void simUpdate(float simTime);
-	int simMain( Menge::SimulatorDBEntry * dbEntry, const std::string & behaveFile,
-			const std::string & sceneFile, const std::string & outFile, const std::string & scbVersion,
-			const std::string & dumpPath ) ;
+protected:
 
-	Menge::Vis::MyViewer _mengeView;
-	const Menge::Agents::SimulatorInterface * _simulator;
-	void TakeControlOnActor();
-	private: std::vector<event::ConnectionPtr> connections;
-        transport::NodePtr node;
-	private: physics::WorldPtr _world;
-	private: double dt;
-	transport::PublisherPtr factoryPub;
-	
-	private: ignition::math::Vector3d velocity;
-	private: double targetWeight = 1.0;
-	private: common::Time lastUpdate;
-	private: physics::TrajectoryInfoPtr trajectoryInfo;
+	virtual void insertAgentModel(const Menge::Agents::BaseAgent* agt);
+	virtual void insertAgentActor(const Menge::Agents::BaseAgent* agt);
+
 };
 GZ_REGISTER_WORLD_PLUGIN(MengeRand)
 } /* namespace gazebo */
